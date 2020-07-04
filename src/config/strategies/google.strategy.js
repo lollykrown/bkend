19ddbefa625c54d98eab9d850ab057b8126339ed
lollykrown <joe_kayu@yahoo.com>
2020-07-04 23:55:02 +0100

    passport-google

---
+++ src/config/strategies/google.strategy.js
@@ -0,0 +1,18 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a token, tokenSecret, and Google profile), and
+//   invoke a callback with a user object.
+passport.use(new GoogleStrategy({
+    consumerKey: GOOGLE_CONSUMER_KEY,
+    consumerSecret: GOOGLE_CONSUMER_SECRET,
+    callbackURL: "http://localhost:4000/auth/google/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return done(err, user);
+      });
+  }
+));
